a69df066f3818182fc07655144ae54ec58c96cea
lydia merrix <lydiamerrix@gmail.com>
2021-12-09 18:58:11 -0500

    added github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lydia Merrix and is{" "}
+        <a href="https://github.com/lydia-merrix/weather-final" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
