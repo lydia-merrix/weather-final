25f61ac071675c3eedb8f4b934dd5b53831d4899
lydia merrix <lydiamerrix@gmail.com>
2021-12-09 23:50:37 -0500

    added css and import info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lydia Merrix and is {" "}
-        <a href="https://github.com/lydia-merrix/weather-final" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        
+        <Weather />
+        <footer>
+          This project was coded by Lydia Merrix and is{" "}
+          <a
+            href="https://github.com/lydia-merrix/weather-final"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
+  
 }
---
+++ src/Weather.js
@@ -0,0 +1,4 @@
+import "./Weather.css";
+export default function Weather() {
+  return <div className="Weather">Hello from Weather</div>;
+}
