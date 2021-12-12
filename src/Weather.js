d91b64422358535ea868ac2a2774d596df4bd682
lydia merrix <lydiamerrix@gmail.com>
2021-12-11 21:58:08 -0500

    deleted let city

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -114,8 +114,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "fd9d9da952d98d244f4e2349d84a75af";
-    let city = "New York";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading....";
